1a56134265573e01c4bbaa7695701b8ecfc5e0c7
Kevin Kelley <kkelley622@gmail.com>
2021-12-14 16:58:11 -0600

    changing h3 element

--- script.js
+++ script.js
@@ -4,6 +4,11 @@ h2.textContent = "I've worked as a travel nurse for 4 years, travleing and livin
 document.querySelector("body").appendChild(h2);
 
 const h3 = document.createElement("h3");
-h3.textContent = "Here is some more information about me";
+h3.textContent = "Here is some more information about me:";
 
 document.querySelector("body").appendChild(h3);
+
+const h5 = document.createElement("h5");
+h3.textContent = "I have 3 Siblings and 5, soon to be 6, niblings!";
+
+document.querySelector("body").appendChild(h5);
