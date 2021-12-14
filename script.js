93ee8c978dda4dc6827af4bda909ff4b19928407
Kevin Kelley <kkelley622@gmail.com>
2021-12-14 15:34:54 -0600

    added personal information to script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+const h2 = document.createElement("h2");
+h2.textContent = "I've worked as a travel nurse for 4 years, travleing and living all over the U.S. But I've realized I need a career change, which is why I'm enrolled in Flatiron School";
+
+document.querySelector("body").appendChild(h2);
+
+const h3 = document.createElement("h3");
+h3.textContent = "Here is some more information about me";
+
+document.querySelector("body").appendChild(h3);
