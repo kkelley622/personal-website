570247bc29b25330af5dc1d3377a14bdf59f7b17
Kevin Kelley <kkelley622@gmail.com>
2021-12-14 17:13:08 -0600

    Completed website

--- script.js
+++ script.js
@@ -1,14 +1,4 @@
 const h2 = document.createElement("h2");
-h2.textContent = "I've worked as a travel nurse for 4 years, travleing and living all over the U.S. But I've realized I need a career change, which is why I'm enrolled in Flatiron School";
+h2.textContent = "I've worked as a travel nurse for 4 years, travleing and living all over the U.S. But I've realized I need a career change, which is why I'm enrolled in Flatiron School. Here is some more information about me:";
 
 document.querySelector("body").appendChild(h2);
-
-const h3 = document.createElement("h3");
-h3.textContent = "Here is some more information about me:";
-
-document.querySelector("body").appendChild(h3);
-
-const h5 = document.createElement("h5");
-h3.textContent = "I have 3 Siblings and 5, soon to be 6, niblings!";
-
-document.querySelector("body").appendChild(h5);
